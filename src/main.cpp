cb271cde5d69f639cc97fad81002e575bbc43613
VanyushaDoronin <ivan.doronin2018@yandex.ru>
2022-10-01 10:54:53 +0300

    develop - added merge sort for lists but with fault

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,19 @@
 #include "clocking.hpp"
 
 int main() {
-    int array[] = {5, 4, 3, 2, 1};
+    int array[] = {6, 7, 8, 9, 112, 5, 3, 4, 1, 2};
     
-    auto source = new array_sequence<int>(array, 5);
-    
+    auto source = new list_sequence<int>(array, 9);
+    std::cout << source;
+    Isorter_list::merge_sort_list(source, compare_int);
+
+    std::cout << source;
+
+    /*auto source = new array_sequence<int>(array, 5);
     std::cout << "Please input sort type\n";
     std::string desision;
     std::cin >> desision;
 
-    clocking_sorts(source, compare_int, desision);
+    clocking_sorts(source, compare_int, desision); */
     return 0;
 }
