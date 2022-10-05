835c893b0f58609cecdfcee17beadfa44de7cf6f
VanyushaDoronin <ivan.doronin2018@yandex.ru>
2022-10-05 22:02:32 +0300

    develop - added tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
+
 #include "ArraySequence.hpp"
 #include "ListSequence.hpp"
 #include "Isorter.hpp"
 #include "clocking.hpp"
+#include "testing.hpp"
 
 int main() {
-    int array[] = {1,3,5,6,8,2,4,7,9,10};
-    
-    auto source = new list_sequence<int>(array, 10);
-    std::cout << source;
-    Isorter_list::merge_sort_list(source, compare_int);
+    if (test_bubble() || test_merge() || test_shaker()) {
+        std::cout << "TESTS FAILED";
+    }
 
-    std::cout << source;
-
-    delete source;
-
-    /*auto source = new array_sequence<int>(array, 5);
+    int array[] = {5, 4, 3, 2, 1};
+    auto source = new array_sequence<int>(array, 5);
     std::cout << "Please input sort type\n";
     std::string desision;
     std::cin >> desision;
 
-    clocking_sorts(source, compare_int, desision); */
+    clocking_sorts(source, compare_int, desision);
     return 0;
 }
---
+++ src/testing.hpp
@@ -0,0 +1,100 @@
+#ifndef SRC_TESTING_HPP__
+#define SRC_TESTING_HPP__
+
+#include <algorithm>
+#include <assert.h>
+#include <chrono>
+#include <stdlib.h>
+#include <vector>
+
+#include "ArraySequence.hpp"
+#include "Isorter.hpp"
+
+bool test_bubble();
+bool test_shaker();
+bool test_merge();
+
+enum test_status {
+    SUCCES = 0,
+    ERROR = 1,
+};
+
+template <typename T1, typename T2>
+bool equal(const T1& first, const T2& second) {
+    assert(first == second);
+    if (first != second)
+        return ERROR;
+    else
+        return SUCCES;
+}
+
+bool test_bubble() {
+    srand(time(NULL));
+
+    vector<int> v;
+    for (std::size_t i = 0; i < 1000; i++) {
+        v.push_back(1 + rand() % RAND_MAX);
+    }
+
+    auto sequence = new array_sequence<int>(4);
+    for (std::size_t i = 0; i < 1000; i++) {
+        sequence->append(v[i]);
+    }
+
+    std::sort(v.begin(), v.end());
+    sequence = Isorter::bubble_sort(sequence, compare_int);
+
+    for (std::size_t i = 0; i < 1000; i++) {
+        if (equal(v[i], sequence->operator[](i))) return ERROR;
+    }
+
+    return SUCCES;
+}
+
+bool test_shaker() {
+    srand(time(NULL));
+
+    vector<int> v;
+    for (std::size_t i = 0; i < 1000; i++) {
+        v.push_back(1 + rand() % RAND_MAX);
+    }
+
+    auto sequence = new array_sequence<int>(4);
+    for (std::size_t i = 0; i < 1000; i++) {
+        sequence->append(v[i]);
+    }
+
+    std::sort(v.begin(), v.end());
+    sequence = Isorter::shaker_sort(sequence, compare_int);
+
+    for (std::size_t i = 0; i < 1000; i++) {
+        if (equal(v[i], sequence->operator[](i))) return ERROR;
+    }
+
+    return SUCCES;
+}
+
+bool test_merge() {
+    srand(time(NULL));
+
+    vector<int> v;
+    for (std::size_t i = 0; i < 1000; i++) {
+        v.push_back(1 + rand() % RAND_MAX);
+    }
+
+    auto sequence = new array_sequence<int>(4);
+    for (std::size_t i = 0; i < 1000; i++) {
+        sequence->append(v[i]);
+    }
+
+    std::sort(v.begin(), v.end());
+    Isorter::merge_sort(sequence, compare_int, 0, sequence->get_size() - 1);
+
+    for (std::size_t i = 0; i < 1000; i++) {
+        if (equal(v[i], sequence->operator[](i))) return ERROR;
+    }
+
+    return SUCCES;
+}
+
+#endif  // SRC_TESTING_HPP__
